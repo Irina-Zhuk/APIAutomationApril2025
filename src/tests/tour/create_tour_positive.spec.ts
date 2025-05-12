70d0517a37f9e90e9d8f73b7ad9ed306011e12ff
Irina-Zhuk <irinamzh@gmail.com>
2025-05-12 16:10:20 -0400

    befor deleting the jest_setup.js

---
+++ src/tests/tour/create_tour_positive.spec.ts
@@ -0,0 +1,41 @@
+import {getUser, signUp} from "../../../helper/user";
+import * as supertest from "supertest";
+const request = supertest('http://localhost:8001/api/v1/')
+
+describe("Create Tour", () => {
+    it("Should create a Tour", async () => {
+        const userImport = getUser ('admin')
+        let cookie;
+        await signUp(userImport).then(res=>{
+            expect(res.statusCode).toBe(201);
+            expect(res.body.data.user.email).toBe(userImport.email.toLowerCase());
+            cookie = res.headers['set-cookie'][0].split('; ')[0]
+        });
+
+        await request
+            .post ('/tours')
+            .set('Cookie', cookie)
+            .send ({
+            name: "TourForn75",
+            duration: 10,
+            description: "Could be",
+            maxGroupSize: 10,
+            summary: "Test tour",
+            difficulty: "easy",
+            price: 100,
+            rating: 4.8,
+            imageCover: "tour-3-cover.jpg",
+            ratingsAverage: 4.9,
+            guides: [],
+            startDates: ["2024-04-04"],
+            startLocation: {
+            type: "Point",
+                coordinates: [-74.005974, 40.712776], // [longitude, latitude]
+        },
+    }).then (tourRes => {
+        expect(tourRes.statusCode).toBe(201)
+        expect (tourRes.body.difficulty).toBe("easy")
+
+    })
+    })
+})
